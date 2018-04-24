c8377b3baea219b4280ccc873c0262204015ade8
Akos Hamori <hamoriakos@gmail.com>
2018-04-24 11:03:11 +0200

    cp: license: replace year
    Signed-off-by: Akos Hamori <hamoriakos@gmail.com>

--- plugins/jsAPI/nodeJsExtension/wrappers/ApeJsBindIndexedFaceSetGeometryImpl.h
+++ plugins/jsAPI/nodeJsExtension/wrappers/ApeJsBindIndexedFaceSetGeometryImpl.h
@@ -1,6 +1,6 @@
 ﻿/*MIT License
 
-Copyright (c) 2016 MTA SZTAKI
+Copyright (c) 2018 MTA SZTAKI
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
