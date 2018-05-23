f133220236966799332ccd1edca86bd3d64e3891
Akos Hamori <akos.hamori@sztaki.mta.hu>
2018-05-23 16:13:17 +0200

    3rdParty: nbind: rename defined variable (cause: conflict)
    Signed-off-by: Akos Hamori <akos.hamori@sztaki.mta.hu>

--- 3rdParty/nbind/include/nbind/nbind.h
+++ 3rdParty/nbind/include/nbind/nbind.h
@@ -7,7 +7,7 @@
 
 #include "noconflict.h"
 
-#define function NBIND_FUNCTION
+#define nfunction NBIND_FUNCTION
 #define multifunction NBIND_MULTIFUNCTION
 #define method(...) NBIND_EXPAND(NBIND_METHOD(__VA_ARGS__))
 #define inherit(...) NBIND_INHERIT(__VA_ARGS__)
