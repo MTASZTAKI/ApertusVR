a8281bdad4c88aa451c2e9963f2688b1151b79d5
Akos Hamori <akos.hamori@sztaki.mta.hu>
2018-05-23 11:27:43 +0200

    3rdParty: add new nan
    Signed-off-by: Akos Hamori <akos.hamori@sztaki.mta.hu>

---
+++ 3rdParty/nan/test/js/weak-test.js
@@ -0,0 +1,36 @@
+/*********************************************************************
+ * NAN - Native Abstractions for Node.js
+ *
+ * Copyright (c) 2018 NAN contributors
+ *
+ * MIT License <https://github.com/nodejs/nan/blob/master/LICENSE.md>
+ ********************************************************************/
+
+const test     = require('tap').test
+    , testRoot = require('path').resolve(__dirname, '..')
+    , bindings = require('bindings')({ module_root: testRoot, bindings: 'weak' });
+
+test('weak', function (t) {
+  t.plan(3);
+
+  var weak = bindings, count = 0;
+  t.type(weak.hustle, 'function');
+
+  weak.hustle(function () {}, function (val) {
+    t.equal(val, 42);
+    count++;
+  });
+
+  var timeout = setTimeout(function () {
+    // run weak callback, should dispose
+    gc();
+
+    // do not run weak callback
+    gc();
+
+    if (count > 0) {
+      clearTimeout(timeout);
+      t.equal(count, 1);
+    }
+  }, 100);
+});
