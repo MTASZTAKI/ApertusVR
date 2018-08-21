e35dd63cfd071ad6f43a4f76655d27e989e8eefa
Akos Hamori <akos.hamori@sztaki.mta.hu>
2018-08-21 13:04:15 +0200

    utilities: add Apertus Package Manager (APM) python script and templates
    Signed-off-by: Akos Hamori <akos.hamori@sztaki.mta.hu>

---
+++ utilities/apm/apm.py
@@ -0,0 +1,154 @@
+from string import Template
+import argparse
+import os
+from distutils.dir_util import copy_tree
+
+uncapitalize = lambda s: s[:1].lower() + s[1:] if s else ''
+capitalize = lambda s: s[:1].upper() + s[1:] if s else ''
+
+class ApePackageManager:
+    variable = "blah"
+
+    def __init__(self):
+        self.templatesPath = './templates/'
+
+    def createPluginTemplate(self, args):
+        pluginName = uncapitalize(args.name)
+        pluginPath = '../../plugins/' + pluginName + '/'
+        pluginClassName = 'Ape' + capitalize(pluginName) + 'Plugin'
+        pluginHeaderName = 'APE_' + pluginClassName.upper() + 'PLUGIN_H'
+
+        # create plugin folder
+        if not os.path.exists(pluginPath):
+            os.makedirs(pluginPath)
+
+        # open plugin header template file
+        pluginHeaderFileName = pluginClassName + '.h'
+        pluginHeaderFilePath = pluginPath + pluginHeaderFileName
+        with open(self.templatesPath + 'plugin_h.template', 'r') as pluginHeaderTemplateFile:
+            data = pluginHeaderTemplateFile.read()
+
+            # replace variables in header template
+            s = Template(data)
+            res = s.substitute(pluginHeaderName=pluginHeaderName, pluginClassName=pluginClassName)
+            # print(res)
+
+            # write plugin header file
+            with open(pluginHeaderFilePath, "w") as pluginHeaderFile:
+                pluginHeaderFile.write(res)
+
+        # open plugin cpp template file
+        pluginCppFileName = pluginClassName + '.cpp'
+        pluginCppFilePath = pluginPath + pluginCppFileName
+        with open(self.templatesPath + 'plugin_cpp.template', 'r') as pluginCppTemplateFile:
+            data = pluginCppTemplateFile.read()
+
+            # replace variables in cpp template
+            s = Template(data)
+            res = s.substitute(pluginClassName=pluginClassName)
+            # print(res)
+
+            # write plugin cpp file
+            with open(pluginCppFilePath, "w") as pluginCppFile:
+                pluginCppFile.write(res)
+
+        # open plugin cmake template file
+        pluginCmakeFilePath = pluginPath + 'CMakeLists.txt'
+        with open(self.templatesPath + 'plugin_cmake.template', 'r') as pluginCmakeTemplateFile:
+            data = pluginCmakeTemplateFile.read()
+
+            # replace variables in cmake template
+            s = Template(data)
+            res = s.substitute(pluginClassName=pluginClassName, pluginHeaderFileName=pluginHeaderFileName, pluginCppFileName=pluginCppFileName)
+            # print(res)
+
+            # write plugin cmake file
+            with open(pluginCmakeFilePath, "w") as pluginCmakeFile:
+                pluginCmakeFile.write(res)
+
+        print(pluginClassName + ' has been created.')
+
+    def createSampleTemplate(self, args):
+        sampleName = uncapitalize(args.name)
+        samplePath = '../../samples/' + sampleName + '/'
+        configPath = samplePath + 'configs/'
+        sampleClassName = 'Ape' + capitalize(sampleName)
+
+        # create sample folder
+        if not os.path.exists(samplePath):
+            os.makedirs(samplePath)
+
+        # open sample cpp template file
+        sampleCppFileName = sampleClassName + '.cpp'
+        sampleCppFilePath = samplePath + sampleCppFileName
+        with open(self.templatesPath + 'sample_cpp.template', 'r') as sampleCppTemplateFile:
+            data = sampleCppTemplateFile.read()
+
+            # replace variables in sample cpp template
+            s = Template(data)
+            res = s.substitute(sampleName=sampleName)
+
+            # write sample cpp file
+            with open(sampleCppFilePath, "w") as sampleCppFile:
+                sampleCppFile.write(res)
+
+        copy_tree(self.templatesPath + 'configs/', samplePath + 'configs/')
+
+        # open sample cmake template file
+        with open(self.templatesPath + 'configs/local_monitor/CMakeLists.txt', 'r') as sampleDefaultConfigCmakeTemplateFile:
+            data = sampleDefaultConfigCmakeTemplateFile.read()
+
+            # replace variables in cmake template
+            s = Template(data)
+            res = s.substitute(sampleName=sampleName)
+
+            # write sample config cmake file
+            with open(configPath + 'local_monitor/' + 'CMakeLists.txt', "w") as sampleDefaultConfigCmakeFile:
+                sampleDefaultConfigCmakeFile.write(res)
+
+        # open sample cmake template file
+        sampleCmakeFilePath = samplePath + 'CMakeLists.txt'
+        with open(self.templatesPath + 'sample_cmake.template', 'r') as sampleCmakeTemplateFile:
+            data = sampleCmakeTemplateFile.read()
+
+            # replace variables in cmake template
+            s = Template(data)
+            res = s.substitute(sampleName=sampleName, sampleClassName=sampleClassName, sampleCppFileName=sampleCppFileName)
+            # print(res)
+
+            # write sample cmake file
+            with open(sampleCmakeFilePath, "w") as sampleCmakeFile:
+                sampleCmakeFile.write(res)
+
+
+apm = ApePackageManager()
+
+parser = argparse.ArgumentParser(description='Optional app description')
+subparsers = parser.add_subparsers(help='sub-command help')
+
+# create the parser for 'status' command
+parser_status = subparsers.add_parser('status', help='a help')
+parser_status.add_argument('bar', type=int, help='bar help')
+
+
+# create the parser for 'create' command
+parser_create = subparsers.add_parser('create', help='b help')
+parser_create_subparsers = parser_create.add_subparsers(help='sub-command help')
+
+# parse plugin arguments
+parser_create_plugin = parser_create_subparsers.add_parser('plugin', help='sub-command help')
+parser_create_plugin.add_argument('-n', '--name', type=str, help='An optional string argument')
+parser_create_plugin.set_defaults(func=apm.createPluginTemplate)
+
+# parse plugin arguments
+parser_create_sample = parser_create_subparsers.add_parser('sample', help='sub-command help')
+parser_create_sample.add_argument('-n', '--name', type=str, help='An optional string argument')
+parser_create_sample.set_defaults(func=apm.createSampleTemplate)
+
+args = parser.parse_args()
+
+print("Argument values:")
+print(args)
+print(args.name)
+
+args.func(args)
