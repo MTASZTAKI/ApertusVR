0b894f9a068b54c0b9f064d08188ef3f098eff8f
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2018-08-22 17:39:25 +0200

    GyorPlugin ligth sources
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- plugins/gyor/ApeGyorPlugin.cpp
+++ plugins/gyor/ApeGyorPlugin.cpp
@@ -31,19 +31,34 @@ void Ape::ApeGyorPlugin::Init()
 		skyBoxMaterial->setFileName("skyBox.material");
 		skyBoxMaterial->setAsSkyBox();
 	}
+	if (auto planeNode = mpScene->createNode("planeNode").lock())
+	{
+		planeNode->setPosition(Ape::Vector3(0, -20, 0));
+		if (auto planeMaterial = std::static_pointer_cast<Ape::IManualMaterial>(mpScene->createEntity("planeMaterial", Ape::Entity::MATERIAL_MANUAL).lock()))
+		{
+			planeMaterial->setDiffuseColor(Ape::Color(0.1f, 0.1f, 0.1f));
+			planeMaterial->setSpecularColor(Ape::Color(0.3f, 0.3f, 0.2f));
+			if (auto plane = std::static_pointer_cast<Ape::IPlaneGeometry>(mpScene->createEntity("plane", Ape::Entity::GEOMETRY_PLANE).lock()))
+			{
+				plane->setParameters(Ape::Vector2(1, 1), Ape::Vector2(1000, 1000), Ape::Vector2(1, 1));
+				plane->setParentNode(planeNode);
+				plane->setMaterial(planeMaterial);
+			}
+		}
+	}
 	if (auto light = std::static_pointer_cast<Ape::ILight>(mpScene->createEntity("light", Ape::Entity::LIGHT).lock()))
 	{
 		light->setLightType(Ape::Light::Type::DIRECTIONAL);
 		light->setLightDirection(Ape::Vector3(1, -1, 0));
-		light->setDiffuseColor(Ape::Color(0.6f, 0.6f, 0.6f));
-		light->setSpecularColor(Ape::Color(0.6f, 0.6f, 0.6f));
+		light->setDiffuseColor(Ape::Color(0.5f, 0.5f, 0.6f));
+		light->setSpecularColor(Ape::Color(0.6f, 0.6f, 0.7f));
 	}
 	if (auto light = std::static_pointer_cast<Ape::ILight>(mpScene->createEntity("light2", Ape::Entity::LIGHT).lock()))
 	{
 		light->setLightType(Ape::Light::Type::DIRECTIONAL);
-		light->setLightDirection(Ape::Vector3(0, -1, 1));
-		light->setDiffuseColor(Ape::Color(0.6f, 0.6f, 0.6f));
-		light->setSpecularColor(Ape::Color(0.6f, 0.6f, 0.6f));
+		light->setLightDirection(Ape::Vector3(0, -1, -1));
+		light->setDiffuseColor(Ape::Color(0.5f, 0.5f, 0.6f));
+		light->setSpecularColor(Ape::Color(0.6f, 0.6f, 0.7f));
 	}
 	LOG_FUNC_LEAVE();
 }
