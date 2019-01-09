1fe54fec6c2b504f8ac573a52047bafbd594e071
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2019-01-09 10:35:49 +0100

    Mulitkinect necessary dependency was added named libjpeg-turbo64
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- 3rdParty/multikinect/libfreenect2-master/cmake_modules/FindLibUSB.cmake
+++ 3rdParty/multikinect/libfreenect2-master/cmake_modules/FindLibUSB.cmake
@@ -32,7 +32,7 @@ ENDIF()
 FIND_PATH(LibUSB_INCLUDE_DIRS
   NAMES libusb.h
   PATHS
-    "${DEPENDS_DIR}/libusb"
+    "${DEPENDS_DIR}/libusb_src/libusb"
     "${DEPENDS_DIR}/libusbx"
     ENV LibUSB_ROOT
   PATH_SUFFIXES
