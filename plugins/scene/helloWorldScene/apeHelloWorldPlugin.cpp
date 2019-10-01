0552e44862a35fd412bc3f1cc609a41dc5379c40
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2019-10-01 11:37:02 +0200

    helloWorldScene
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- plugins/scene/helloWorldScene/apeHelloWorldPlugin.cpp
+++ plugins/scene/helloWorldScene/apeHelloWorldPlugin.cpp
@@ -28,33 +28,44 @@ void ape::apeHelloWorldScenePlugin::eventCallBack(const ape::Event& event)
 void ape::apeHelloWorldScenePlugin::Init()
 {
 	APE_LOG_FUNC_ENTER();
-	//mpSceneMakerMacro->makeLit();
+	if (auto universeSkyBoxMaterial = std::static_pointer_cast<ape::IFileMaterial>(mpSceneManager->createEntity("universe", ape::Entity::MATERIAL_FILE).lock()))
+	{
+		universeSkyBoxMaterial->setFileName("universe.material");
+		universeSkyBoxMaterial->setAsSkyBox();
+	}
 	if (auto light = std::static_pointer_cast<ape::ILight>(mpSceneManager->createEntity("light", ape::Entity::LIGHT).lock()))
 	{
 		light->setLightType(ape::Light::Type::DIRECTIONAL);
-		light->setLightDirection(ape::Vector3(1, -1, 0));
-		light->setDiffuseColor(ape::Color(0.6f, 0.6f, 0.6f));
-		light->setSpecularColor(ape::Color(0.6f, 0.6f, 0.6f));
+		light->setLightDirection(ape::Vector3(1, 0, 0));
+		light->setDiffuseColor(ape::Color(0.8f, 0.8f, 0.8f));
+		light->setSpecularColor(ape::Color(0.8f, 0.8f, 0.8f));
 	}
-	if (auto light = std::static_pointer_cast<ape::ILight>(mpSceneManager->createEntity("light2", ape::Entity::LIGHT).lock()))
+	mPlanetNode = mpSceneManager->createNode("planetNode");
+	if (auto planetNode = mPlanetNode.lock())
 	{
-		light->setLightType(ape::Light::Type::DIRECTIONAL);
-		light->setLightDirection(ape::Vector3(0, -1, 1));
-		light->setDiffuseColor(ape::Color(0.6f, 0.6f, 0.6f));
-		light->setSpecularColor(ape::Color(0.6f, 0.6f, 0.6f));
+		planetNode->setPosition(ape::Vector3(0, 0, -100));
+		if (auto planetMeshFile = std::static_pointer_cast<ape::IFileGeometry>(mpSceneManager->createEntity("planet.mesh", ape::Entity::GEOMETRY_FILE).lock()))
+		{
+			planetMeshFile->setFileName("planet.mesh");
+			planetMeshFile->setParentNode(planetNode);
+		}
+		if (auto textNode = mpSceneManager->createNode("helloWorldText_Node").lock())
+		{
+			textNode->setParentNode(planetNode);
+			textNode->setPosition(ape::Vector3(0.0f, 10.0f, 0.0f));
+			if (auto userNameText = std::static_pointer_cast<ape::ITextGeometry>(mpSceneManager->createEntity("helloWorldText", ape::Entity::GEOMETRY_TEXT).lock()))
+			{
+				userNameText->setCaption("helloWorld");
+				userNameText->setParentNode(textNode);
+			}
+		}
 	}
-	mpSceneMakerMacro->makeBackground();
-	mpSceneMakerMacro->makeGround();
 	APE_LOG_FUNC_LEAVE();
 }
 
 void ape::apeHelloWorldScenePlugin::Run()
 {
 	APE_LOG_FUNC_ENTER();
-	while (true)
-	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(20));
-	}
 	APE_LOG_FUNC_LEAVE();
 }
 
