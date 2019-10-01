2658c92ce61580640bd221499db7d61507669295
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2019-10-01 13:03:53 +0200

    3rdParties build folder is in the user selected output folder
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- core/sceneManager/network/prepare_raknet.cmake
+++ core/sceneManager/network/prepare_raknet.cmake
@@ -6,9 +6,11 @@
 # raknet patch log end
 
 set (RAKNET_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/network/3rdParty/raknet)
-set (RAKNET_HOME ${RAKNET_SRC_DIR})
-set (RAKNET_BUILD_DIR ${RAKNET_SRC_DIR}/build)
-set (RAKNET_INSTALL_DIR ${RAKNET_SRC_DIR}/install)
+if (NOT EXISTS ${CMAKE_BINARY_DIR}/raknet/build)
+		file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/raknet/build)
+endif()
+set (RAKNET_BUILD_DIR ${CMAKE_BINARY_DIR}/raknet/build)
+set (RAKNET_INSTALL_DIR ${CMAKE_BINARY_DIR}/raknet/install)
 set (RAKNET_READY FALSE)
 
 # solve a *nix/64bit processor speciality
@@ -19,16 +21,16 @@ endif ()
 # find raknet
 if (WIN32)
 	message (STATUS "RakNet: prepare on Windows")
-	set(RAKNET_STATIC_LIB_PATH_DEBUG "${RAKNET_HOME}/Lib/LibStatic/Debug/RakNetLibStatic.lib")
-	set(RAKNET_STATIC_LIB_PATH_RELEASE "${RAKNET_HOME}/Lib/LibStatic/Release/RakNetLibStatic.lib")
+	set(RAKNET_STATIC_LIB_PATH_DEBUG "${RAKNET_BUILD_DIR}/Lib/LibStatic/Debug/RakNetLibStatic.lib")
+	set(RAKNET_STATIC_LIB_PATH_RELEASE "${RAKNET_BUILD_DIR}/Lib/LibStatic/Release/RakNetLibStatic.lib")
 elseif (APPLE)
 	message (STATUS "RakNet: prepare on Apple")
-	set(RAKNET_STATIC_LIB_PATH_DEBUG "${RAKNET_HOME}/Lib/LibStatic/Debug/libRakNetLibStatic.a")
-	set(RAKNET_STATIC_LIB_PATH_RELEASE "${RAKNET_HOME}/Lib/LibStatic/Release/libRakNetLibStatic.a")
+	set(RAKNET_STATIC_LIB_PATH_DEBUG "${RAKNET_BUILD_DIR}/Lib/LibStatic/Debug/libRakNetLibStatic.a")
+	set(RAKNET_STATIC_LIB_PATH_RELEASE "${RAKNET_BUILD_DIR}/Lib/LibStatic/Release/libRakNetLibStatic.a")
 elseif (LINUX)
 	message (STATUS "RakNet: prepare on Linux")
-	set(RAKNET_STATIC_LIB_PATH_DEBUG "${RAKNET_HOME}/Lib/LibStatic/libRakNetLibStatic.a")
-	set(RAKNET_STATIC_LIB_PATH_RELEASE "${RAKNET_HOME}/Lib/LibStatic/libRakNetLibStatic.a")
+	set(RAKNET_STATIC_LIB_PATH_DEBUG "${RAKNET_BUILD_DIR}/Lib/LibStatic/libRakNetLibStatic.a")
+	set(RAKNET_STATIC_LIB_PATH_RELEASE "${RAKNET_BUILD_DIR}/Lib/LibStatic/libRakNetLibStatic.a")
 endif ()
 
 message (STATUS "RakNet: RAKNET_STATIC_LIB_PATH_DEBUG: ${RAKNET_STATIC_LIB_PATH_DEBUG}")
@@ -58,14 +60,14 @@ if (NOT RAKNET_READY)
 				-DCMAKE_OSX_ARCHITECTURES=${DS_OSX_ARCHITECTURE}
 				-G ${CMAKE_GENERATOR}
 				${RAKNET_SRC_DIR}
-				WORKING_DIRECTORY ${RAKNET_SRC_DIR}
+				WORKING_DIRECTORY ${RAKNET_BUILD_DIR}
 				RESULT_VARIABLE error OUTPUT_VARIABLE output ERROR_VARIABLE output
 				)
 		reportError (output if error)
 		message (STATUS "Building raknet for ${CONF}, please wait.....")
 		execute_process (
-			COMMAND ${CMAKE_COMMAND} --build ${RAKNET_SRC_DIR} --config ${CONF} --target RakNetLibStatic
-			WORKING_DIRECTORY ${RAKNET_SRC_DIR}
+			COMMAND ${CMAKE_COMMAND} --build ${RAKNET_BUILD_DIR} --config ${CONF} --target RakNetLibStatic
+			WORKING_DIRECTORY ${RAKNET_BUILD_DIR}
 			RESULT_VARIABLE error OUTPUT_VARIABLE output ERROR_VARIABLE output)
 		reportError (output if error)
 	endforeach ()
@@ -75,4 +77,4 @@ endif ()
 add_library(RAKNET STATIC IMPORTED)
 set_property(TARGET RAKNET PROPERTY IMPORTED_LOCATION_DEBUG ${RAKNET_STATIC_LIB_PATH_DEBUG})
 set_property(TARGET RAKNET PROPERTY IMPORTED_LOCATION_RELEASE ${RAKNET_STATIC_LIB_PATH_RELEASE})
-set(RAKNET_INCLUDE_DIR ${RAKNET_HOME}/Source)
+set(RAKNET_INCLUDE_DIR ${RAKNET_SRC_DIR}/Source)
