c34cbfd3a8f34bc0f97218a1dcd158cb010c207f
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2019-10-08 13:50:50 +0200

    industry40 sample OK
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- plugins/languageAPI/jsAPI/nodeJsPlugin/js/plugins/httpSimulator/httpSimulator.js
+++ plugins/languageAPI/jsAPI/nodeJsPlugin/js/plugins/httpSimulator/httpSimulator.js
@@ -1,17 +1,13 @@
 /*MIT License
-
 Copyright (c) 2017 MTA SZTAKI
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,7 +24,7 @@ var LineByLineReader = moduleManager.requireNodeModule('line-by-line');
 class HttpSimulator {
 	constructor(options) {
 		this.options = {
-			uri: moduleManager.httpServer.address + '/api/v1/settensorflow',
+			uri: moduleManager.httpServer.address + '/api/v1/setproperties',
 			method: 'POST',
 			json: {}
 		};
@@ -46,7 +42,7 @@ class HttpSimulator {
 		this.delay = args.delay || 20;
 
 		this.lr = new LineByLineReader(this.fileName);
-		
+
 		this.lr.on('error', function (err) {
 			console.log('line-reader: error: ', err);
 			self.lr = {};
@@ -55,7 +51,7 @@ class HttpSimulator {
 
 		this.lr.on('line', function (line) {
 			self.lr.pause();
-			try{
+			try {
 				self.options.json = JSON.parse(line);
 			}
 			catch (error) {
@@ -66,16 +62,16 @@ class HttpSimulator {
 
 			// this.lineNumber++;
 			// console.log('line-reader: line> ', this.lineNumber);
-			/*request.post(self.options, function (error, response, body) {
-					if (error) {
-						console.log('http post error: ', error);
-					}
-					// console.log(JSON.stringify(body));
-					setTimeout(function(){
-						self.lr.resume();
-					}, self.delay);
+			request.post(self.options, function (error, response, body) {
+				if (error) {
+					console.log('http post error: ', error);
 				}
-			);*/
+				// console.log(JSON.stringify(body));
+				setTimeout(function () {
+					self.lr.resume();
+				}, self.delay);
+			}
+			);
 		});
 
 		this.lr.on('end', function () {
