9921e6ef10bd0128035de97f203f389b5bf0047a
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2020-01-31 14:42:47 +0100

    WebSocketServerPlugin create,listen,connect and open a websocket is working properly
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

---
+++ plugins/languageAPI/jsAPI/nodeJsPlugin/js/plugins/virtualLearningFactoryUI/public/js/script.js
@@ -0,0 +1,49 @@
+var apiEndPoint = 'http://localhost:3000/api/v1/';
+var apiEndPointNode = apiEndPoint + 'nodes/';
+
+
+function getNodesNames() {
+	console.log('getNodesNames()');
+	doGetRequest(apiEndPointNode, function (res) {
+		var nodeNames = res.data.items;
+		console.log('getNodesNames(): res: ', nodeNames);
+
+		nodeNames.forEach(function (element) {
+			genNodeItem(element.name);
+		});
+	});
+}
+
+function doGetRequest(apiEndPointUrl, callback) {
+    $.get(apiEndPointUrl, function(res) {
+        // console.log('doGetRequest(): ', res);
+        callback(res);
+    });
+}
+
+function doPostRequest(apiEndPointUrl, data, callback) {
+    $.post(apiEndPointUrl, data, function(res) {
+        // console.log('doPostRequest(): ', res);
+        callback(res);
+    }, "json");
+}
+
+
+$(document).ready(function(){
+    var sock = new WebSocket("ws://localhost:40080/ws");
+    sock.onopen = ()=>{
+        console.log('open')
+    }
+    sock.onerror = (e)=>{
+        console.log('error',e)
+    }
+    sock.onclose = ()=>{
+        console.log('close')
+    }
+    sock.onmessage = (e)=>{
+        console.log('onmessage:' + e.data);
+        var eventObj = JSON.parse(e.data);
+
+        console.log('eventObj: ', eventObj);
+    }
+});
