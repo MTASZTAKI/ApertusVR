f7b43f6ec4690ee18708cd27aa1f95d448b6886a
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2020-04-16 20:47:55 +0200

    VLFT, fileGeom replica
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

--- plugins/virtualLearningFactory/sceneLoader/apeVLFTSceneLoaderPlugin.cpp
+++ plugins/virtualLearningFactory/sceneLoader/apeVLFTSceneLoaderPlugin.cpp
@@ -45,7 +45,7 @@ void ape::VLFTSceneLoaderPlugin::parseRepresentations()
 					{
 						node->setVisible(false);
 						float unitScale = *representation.get_unit() / 0.01f;
-						if (auto fileGeometry = std::static_pointer_cast<ape::IFileGeometry>(mpSceneManager->createEntity(asset.get_id(), ape::Entity::Type::GEOMETRY_FILE, false, mpCoreConfig->getNetworkGUID()).lock()))
+						if (auto fileGeometry = std::static_pointer_cast<ape::IFileGeometry>(mpSceneManager->createEntity(asset.get_id(), ape::Entity::Type::GEOMETRY_FILE, true, mpCoreConfig->getNetworkGUID()).lock()))
 						{
 							//APE_LOG_DEBUG("fileGeometry: " << asset.get_id() << " filename: " << filePath);
 							fileGeometry->setUnitScale(unitScale);
