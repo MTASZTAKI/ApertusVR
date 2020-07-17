2441cdec0869cd14d86811be5bb4f1c4e13d4a18
pkovacs86 <peter.kovacs@sztaki.mta.hu>
2020-07-17 10:59:25 +0200

    Collision Detection, websocket and listener apps added
    Signed-off-by: pkovacs86 <peter.kovacs@sztaki.mta.hu>

---
+++ samples/collisionDetection/listener/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    var sock = new WebSocket("ws://localhost:40080/ws");
+	sock.onopen = () => {
+		console.log('open');
+    }
+    sock.onerror = (e)=>{
+        console.log('error',e)
+    }
+    sock.onclose = ()=>{
+        console.log('close')
+    }
+    sock.onmessage = (e)=>{
+        console.log('onmessage:' + e.data);
+        var eventObj = JSON.parse(e.data);
+        console.log('eventObj: ', eventObj);
+        if (eventObj.type == 6410/*RIGIDBODY_COLLISION*/) {
+            var messagesSelect = document.getElementById('messagesSelect');
+            var newMsg = document.createElement('msg');
+            newMsg.innerHTML = eventObj.subjectName;
+            messagesSelect.appendChild(newMsg);
+        }
+    }
+});
